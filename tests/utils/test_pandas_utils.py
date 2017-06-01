741a5b519403fbda21cdbd45bc6d7789bfafdcbe
Ana Ruelas <aruelas@quantopian.com>
2017-06-01 12:20:15 -0400

    TST: Assert error messages are what is expected

--- tests/utils/test_pandas_utils.py
+++ tests/utils/test_pandas_utils.py
@@ -173,8 +173,16 @@ class TestCatDFConcat(ZiplineTestCase):
             ),
         ]
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as cm:
             categorical_df_concat(mismatched_dtypes)
+        self.assertEqual(
+            cm.exception.message,
+            "Input DataFrames must have the same columns/dtypes."
+        )
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as cm:
             categorical_df_concat(mismatched_column_names)
+        self.assertEqual(
+            cm.exception.message,
+            "Input DataFrames must have the same columns/dtypes."
+        )
